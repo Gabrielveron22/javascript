ba4b85c2c3e612074bf50e93937f3a754cee1797
Gabriel Alves <94207260+Gabrielveron22@users.noreply.github.com>
2022-03-07 19:20:11 -0300

    Retirando bugs

---
+++ Projeto Calculadora darkMode/script.js
@@ -0,0 +1,33 @@
+function insert(num) 
+{
+ var numero = document.getElementById('resultado').innerHTML
+ document.getElementById('resultado').innerHTML = numero + num
+}
+
+function clean(){
+    document.getElementById('resultado').innerHTML = ""
+}
+
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring (0, resultado.length -1)
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado ){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
+
+// DARK MODE //
+const fundo = document.querySelector('.fundo')
+const h1 = document.querySelector('h1')
+const h12 = document.querySelector('.h1')
+const toggle = document.getElementById('toggle')
+toggle.onclick = function(){
+    toggle.classList.toggle('active')
+    fundo.classList.toggle('active')
+    h1.classList.toggle('active')
+    h12.classList.toggle('active')
+}
